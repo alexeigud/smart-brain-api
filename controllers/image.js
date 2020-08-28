e70a44e10e83090e4f1e4c53356432496b2eb931
Alexei <alexgud300@gmail.com>
2020-08-28 09:03:19 -0400

    more typos2

--- controllers/image.js
+++ controllers/image.js
@@ -14,7 +14,7 @@ const handleApiCall = (req, res) => {
 }
 
 
-const handleImage  = (req, res, db) =>{
+const handleImage = (req, res, db) =>{
     const { id } = req.body;
     db('users').where('id', '=', id)
     .increment('entries', 1)
